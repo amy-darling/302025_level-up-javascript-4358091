886072a296b4e4c2a58b89372eef60489708ded8
amy-darling <amy.darling@avalerehealth.com>
2025-03-19 20:57:41 +0000

    movie prototype challenge

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,26 @@
 // Write your code here
+class Movie {
+  constructor(title, director, genre, releaseYear, rating) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+
+  get overview() {
+    return this.getOverview();
+  }
+
+  getOverview() {
+    return `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`;
+  }
+
+}
+
+const Magnolia = new Movie ("Magnolia", "Paul Thomas Anderson", "Classic Drama", 1999, 80);
+const LuisWain = new Movie ("The Electrical Life of Louis Wain", "Will Sharpe", "Melodrama", 2021, 68);
+
+console.log(Magnolia);
+console.log(Magnolia.getOverview());
+console.log(LuisWain.getOverview());
